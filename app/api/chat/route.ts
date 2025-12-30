eb68f2efe7ebd82088800d2ac3740ed13021b29e
Utkarsh Chaudhary <112180893+Utkarshchaudhary009@users.noreply.github.com>
2025-12-30 09:37:43 +0000

    Refactor code structure for improved readability and maintainability

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -2,6 +2,11 @@ import { google } from "@ai-sdk/google";
 import { z } from "zod";
 import { streamText, UIMessage, convertToModelMessages } from "ai";
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
+import { cerebras } from "@ai-sdk/cerebras";
+import { groq } from "@ai-sdk/groq";
+import { xai } from "@ai-sdk/xai";
+import { huggingface } from "@ai-sdk/huggingface";
+import { perplexity } from "@ai-sdk/perplexity";
 import {
   ToolLoopAgent,
   createAgentUIStreamResponse,
@@ -14,9 +19,7 @@ const openrouter = createOpenRouter({
 });
 
 export const maxDuration = 30;
-const Google_Provider = google("gemini-2.5-flash-lite-preview-09-2025");
 const Openrouter = openrouter.chat("anthropic/claude-3.5-sonnet");
-
 export async function POST(req: Request) {
   const {
     messages,
@@ -27,10 +30,42 @@ export async function POST(req: Request) {
     model: string;
     webSearch: boolean;
   } = await req.json();
-
+  let Agentmodel;
+  if (model.includes("qwen")) {
+    Agentmodel = cerebras.chat("qwen-3-235b-a22b-thinking-2507");
+  } else if (model.includes("llama")) {
+    Agentmodel = groq("meta-llama/llama-guard-4-12b");
+  } else if (model.includes("grok")) {
+    Agentmodel = xai("grok-3-mini-fast");
+  } else if (model.includes("oss")) {
+    Agentmodel = huggingface("openai/gpt-oss-20b:cheapest");
+  } else {
+    Agentmodel = google("gemini-2.5-flash-lite-preview-09-2025");
+  }
+  if (webSearch) {
+    const searchModel=["g","g","g","p"];
+    if (searchModel[Math.floor(Math.random() * searchModel.length)] === "g") {
+    const result = streamText({
+      model: google("gemini-2.5-flash-lite-preview-09-2025"),
+      messages: await convertToModelMessages(messages),
+      tools: {
+        google_search: google.tools.googleSearch({}),
+        url_context: google.tools.urlContext({}),
+      },
+      system:
+        "You are a helpful assistant that can answer questions and help with tasks",
+    });
+    // send sources and reasoning back to the client
+    return result.toUIMessageStreamResponse({
+      sendSources: true,
+      sendReasoning: true,
+    });}
+    else {
+    Agentmodel = perplexity("sonar");}
+  }
   if (model.includes("free")) {
     const result = streamText({
-      model: openrouter.chat(model) || Google_Provider, // or gemini-1.5-flash
+      model: openrouter.chat(model), // or gemini-1.5-flash
       messages: await convertToModelMessages(messages),
       system:
         "You are a helpful assistant that can answer questions and help with tasks",
@@ -42,7 +77,7 @@ export async function POST(req: Request) {
     });
   } else {
     const technicalWriterAgent = new ToolLoopAgent({
-      model: Google_Provider,
+      model: Agentmodel,
       instructions: `You are a technical documentation writer.
 
   Writing style:
@@ -70,6 +105,8 @@ export async function POST(req: Request) {
     return createAgentUIStreamResponse({
       agent: technicalWriterAgent as any,
       uiMessages: messages,
+      sendSources: true,
+      sendReasoning: true,
     });
   }
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 import {
   Conversation,
   ConversationContent,
   ConversationScrollButton,
-} from '@/components/ai-elements/conversation';
+} from "@/components/ai-elements/conversation";
 import {
   Message,
   MessageContent,
   MessageResponse,
   MessageActions,
   MessageAction,
-} from '@/components/ai-elements/message';
+} from "@/components/ai-elements/message";
 import {
   PromptInput,
   PromptInputActionAddAttachments,
@@ -32,66 +32,113 @@ import {
   PromptInputTextarea,
   PromptInputFooter,
   PromptInputTools,
-} from '@/components/ai-elements/prompt-input';
-import { Fragment, useState } from 'react';
-import { useChat } from '@ai-sdk/react';
-import { CopyIcon, GlobeIcon, RefreshCcwIcon } from 'lucide-react';
+} from "@/components/ai-elements/prompt-input";
+
+import { Suggestion, Suggestions } from "@/components/ai-elements/suggestion";
+
+import {
+  Tool,
+  ToolContent,
+  ToolHeader,
+  ToolInput,
+  ToolOutput,
+} from "@/components/ai-elements/tool";
+import { Fragment, useState } from "react";
+import { useChat } from "@ai-sdk/react";
+import { CopyIcon, GlobeIcon, RefreshCcwIcon } from "lucide-react";
 import {
   Source,
   Sources,
   SourcesContent,
   SourcesTrigger,
-} from '@/components/ai-elements/sources';
+} from "@/components/ai-elements/sources";
 import {
   Reasoning,
   ReasoningContent,
   ReasoningTrigger,
-} from '@/components/ai-elements/reasoning';
-import { Loader } from '@/components/ai-elements/loader';
+} from "@/components/ai-elements/reasoning";
+import { Shimmer } from "@/components/ai-elements/shimmer";
 const models = [
-
+  
   {
-    name: 'Gemini 2.5 Flash-Lite Preview 09-2025 (Google)',
-    value: 'gemini-2.5-flash-lite-preview-09-2025',
+    name: "DeepSeek R1",
+    value: "deepseek/deepseek-r1-0528:free",
   },
   {
-    name: 'TNG: R1T Chimera (free)',
-    value: 'tngtech/tng-r1t-chimera:free',
+    name: "Meta Llama Guard 4",
+    value: "meta-llama/llama-guard-4-12b",
   },
   {
-    name: 'DeepSeek: R1 0528 (free)',
-    value: 'deepseek/deepseek-r1-0528:free',
+    name: "Gemini 2.5 Flash-Lite",
+    value: "gemini-2.5-flash-lite-preview-09-2025",
   },
   {
-    name: 'TNG: DeepSeek R1T Chimera (free)',
-    value: 'tngtech/deepseek-r1t-chimera:free',
+    name: "Gemini 2.5 Flash",
+    value: "gemini-2.5-flash",
   },
   {
-    name: 'Nex AGI: DeepSeek V3.1 Nex N1 (free)',
-    value: 'nex-agi/deepseek-v3.1-nex-n1:free',
+    name: "Qwen 3",
+    value: "qwen-3-32b",
   },
   {
-    name: 'OpenAI: gpt-oss-20b (free)',
-    value: 'openai/gpt-oss-20b:free',
+    name: "TNG: R1T Chimera",
+    value: "tngtech/tng-r1t-chimera:free",
   },
   {
-    name: 'OpenAI: gpt-oss-120b (free)',
-    value: 'openai/gpt-oss-120b:free',
+    name: "TNG: DeepSeek R1T Chimera",
+    value: "tngtech/deepseek-r1t-chimera:free",
   },
   {
-    name: 'Google: Gemma 3 27B (free)',
-    value: 'google/gemma-3-27b-it:free',
+    name: "DeepSeek V3.1 Nex N1",
+    value: "nex-agi/deepseek-v3.1-nex-n1:free",
   },
   {
-    name: 'AllenAI: Olmo 3.1 32B Think (free)',
-    value: 'allenai/olmo-3.1-32b-think:free',
+    name: "gpt-oss-20b",
+    value: "openai/gpt-oss-20b:free",
+  },
+  {
+    name: "Grok 3 Mini",
+    value: "grok-3-mini-fast",
+  },
+  {
+    name: "GPT-OSS 20B",
+    value: "openai/gpt-oss-20b:cheapest",
+  },
+  {
+    name: "AllenAI: Olmo 3.1 32B Think",
+    value: "allenai/olmo-3.1-32b-think:free",
   },
 ];
+const suggestions = [
+  "What are the latest trends in AI?",
+  "How does machine learning work?",
+  "Explain quantum computing",
+  "Best practices for React development",
+  "Tell me about TypeScript benefits",
+  "How to optimize database queries?",
+  "What is the difference between SQL and NoSQL?",
+  "Explain cloud computing basics",
+];
+
 const ChatBotDemo = () => {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [model, setModel] = useState<string>(models[0].value);
   const [webSearch, setWebSearch] = useState(false);
   const { messages, sendMessage, status, regenerate } = useChat();
+  const handleSuggestionClick = (suggestion: string) => {
+   sendMessage(
+      {
+        text: suggestion
+      },
+      {
+        body: {
+          model: model,
+          webSearch: webSearch,
+        },
+      }
+    );
+
+  };
   const handleSubmit = (message: PromptInputMessage) => {
     const hasText = Boolean(message.text);
     const hasAttachments = Boolean(message.files?.length);
@@ -99,18 +146,18 @@ const ChatBotDemo = () => {
       return;
     }
     sendMessage(
-      { 
-        text: message.text || 'Sent with attachments',
-        files: message.files 
+      {
+        text: message.text || "Sent with attachments",
+        files: message.files,
       },
       {
         body: {
           model: model,
           webSearch: webSearch,
         },
-      },
+      }
     );
-    setInput('');
+    setInput("");
   };
   return (
     <div className="max-w-4xl mx-auto p-6 relative size-full h-screen">
@@ -119,62 +166,84 @@ const ChatBotDemo = () => {
           <ConversationContent>
             {messages.map((message) => (
               <div key={message.id}>
-                {message.role === 'assistant' && message.parts.filter((part) => part.type === 'source-url').length > 0 && (
-                  <Sources>
-                    <SourcesTrigger
-                      count={
-                        message.parts.filter(
-                          (part) => part.type === 'source-url',
-                        ).length
-                      }
-                    />
-                    {message.parts.filter((part) => part.type === 'source-url').map((part, i) => (
-                      <SourcesContent key={`${message.id}-${i}`}>
-                        <Source
-                          key={`${message.id}-${i}`}
-                          href={part.url}
-                          title={part.url}
-                        />
-                      </SourcesContent>
-                    ))}
-                  </Sources>
-                )}
+                {message.role === "assistant" &&
+                  message.parts.filter((part) => part.type === "source-url")
+                    .length > 0 && (
+                    <Sources>
+                      <SourcesTrigger
+                        count={
+                          message.parts.filter(
+                            (part) => part.type === "source-url"
+                          ).length
+                        }
+                      />
+                      {message.parts
+                        .filter((part) => part.type === "source-url")
+                        .map((part, i) => (
+                          <SourcesContent key={`${message.id}-${i}`}>
+                            <Source
+                              key={`${message.id}-${i}`}
+                              href={part.url}
+                              title={part.url}
+                            />
+                          </SourcesContent>
+                        ))}
+                    </Sources>
+                  )}
                 {message.parts.map((part, i) => {
+                  if (part.type.includes("tool-")) {
+                    return (
+                      <div style={{ height: "500px" }}>
+                        <Tool>
+                          {/* <ToolHeader
+                            state={toolCall.state}
+                            type={toolCall.type}
+                          />
+                          <ToolContent>
+                            <ToolInput input={toolCall.input} />
+                          </ToolContent> */}
+                        </Tool>
+                      </div>
+                    );
+                  }
                   switch (part.type) {
-                    case 'text':
+                    case "text":
                       return (
                         <Message key={`${message.id}-${i}`} from={message.role}>
                           <MessageContent>
-                            <MessageResponse>
-                              {part.text}
-                            </MessageResponse>
+                            <MessageResponse>{part.text}</MessageResponse>
                           </MessageContent>
-                          {message.role === 'assistant' && i === messages.length - 1 && (
-                            <MessageActions>
-                              <MessageAction
-                                onClick={() => regenerate()}
-                                label="Retry"
-                              >
-                                <RefreshCcwIcon className="size-3" />
-                              </MessageAction>
-                              <MessageAction
-                                onClick={() =>
-                                  navigator.clipboard.writeText(part.text)
-                                }
-                                label="Copy"
-                              >
-                                <CopyIcon className="size-3" />
-                              </MessageAction>
-                            </MessageActions>
-                          )}
+                          {message.role === "assistant" &&
+                            i === messages.length - 1 && (
+                              <MessageActions>
+                                <MessageAction
+                                  onClick={() => regenerate()}
+                                  label="Retry"
+                                >
+                                  <RefreshCcwIcon className="size-3" />
+                                </MessageAction>
+                                <MessageAction
+                                  onClick={() =>
+                                    navigator.clipboard.writeText(part.text)
+                                  }
+                                  label="Copy"
+                                >
+                                  <CopyIcon className="size-3" />
+                                </MessageAction>
+                              </MessageActions>
+                            )}
                         </Message>
                       );
-                    case 'reasoning':
+                    case "reasoning":
                       return (
                         <Reasoning
                           key={`${message.id}-${i}`}
                           className="w-full"
-                          isStreaming={status === 'streaming' && i === message.parts.length - 1 && message.id === messages.at(-1)?.id}
+                          isStreaming={
+                            status === "streaming" &&
+                            i === message.parts.length - 1 &&
+                            message.id === messages.at(-1)?.id
+                          }
                         >
                           <ReasoningTrigger />
                           <ReasoningContent>{part.text}</ReasoningContent>
@@ -186,11 +255,25 @@ const ChatBotDemo = () => {
                 })}
               </div>
             ))}
-            {status === 'submitted' && <Loader />}
+            {status === "submitted" && <Shimmer>loading...</Shimmer>}
           </ConversationContent>
           <ConversationScrollButton />
         </Conversation>
-        <PromptInput onSubmit={handleSubmit} className="mt-4" globalDrop multiple>
+      <Suggestions>
+        {suggestions.map((suggestion) => (
+          <Suggestion
+            key={suggestion}
+            onClick={handleSuggestionClick}
+            suggestion={suggestion}
+          />
+        ))}
+      </Suggestions>
+        <PromptInput
+          onSubmit={handleSubmit}
+          className="mt-4"
+          globalDrop
+          multiple
+        >
           <PromptInputHeader>
             <PromptInputAttachments>
               {(attachment) => <PromptInputAttachment data={attachment} />}
@@ -211,7 +294,7 @@ const ChatBotDemo = () => {
                 </PromptInputActionMenuContent>
               </PromptInputActionMenu>
               <PromptInputButton
-                variant={webSearch ? 'default' : 'ghost'}
+                variant={webSearch ? "default" : "ghost"}
                 onClick={() => setWebSearch(!webSearch)}
               >
                 <GlobeIcon size={16} />
@@ -228,7 +311,10 @@ const ChatBotDemo = () => {
                 </PromptInputSelectTrigger>
                 <PromptInputSelectContent>
                   {models.map((model) => (
-                    <PromptInputSelectItem key={model.value} value={model.value}>
+                    <PromptInputSelectItem
+                      key={model.value}
+                      value={model.value}
+                    >
                       {model.name}
                     </PromptInputSelectItem>
                   ))}
